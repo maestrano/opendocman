a3c8c2035bf0953ec99dbecda5358a922e0bfee5
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-02-20 18:26:21 +1100

    configured api

--- maestrano/app/sso/MnoSsoUser.php
+++ maestrano/app/sso/MnoSsoUser.php
@@ -37,29 +37,17 @@ class MnoSsoUser extends MnoSsoBaseUser
    *
    * @return boolean whether the user was successfully set in session or not
    */
-  // protected function setInSession()
-  // {
-  //   // First set $conn variable (need global variable?)
-  //   $conn = $this->connection;
-  //   
-  //   $sel1 = $conn->query("SELECT ID,name,lastlogin FROM user WHERE ID = $this->local_id");
-  //   $chk = $sel1->fetch();
-  //   if ($chk["ID"] != "") {
-  //       $now = time();
-  //       
-  //       // Set session
-  //       $this->session['userid'] = $chk['ID'];
-  //       $this->session['username'] = stripslashes($chk['name']);
-  //       $this->session['lastlogin'] = $now;
-  //       
-  //       // Update last login timestamp
-  //       $upd1 = $conn->query("UPDATE user SET lastlogin = '$now' WHERE ID = $this->local_id");
-  //       
-  //       return true;
-  //   } else {
-  //       return false;
-  //   }
-  // }
+  protected function setInSession()
+  {
+    if ($this->local_id) {
+        // initiate a session
+        $_SESSION['uid'] = $this->local_id;
+        
+        return true;
+    } else {
+        return false;
+    }
+  }
   
   
   /**
@@ -69,52 +57,142 @@ class MnoSsoUser extends MnoSsoBaseUser
    *
    * @return the ID of the user created, null otherwise
    */
-  // protected function createLocalUser()
-  // {
-  //   $lid = null;
-  //   
-  //   if ($this->accessScope() == 'private') {
-  //     // First set $conn variable (need global variable?)
-  //     $conn = $this->connection;
-  //     
-  //     // Create user
-  //     $lid = $this->connection->query("CREATE BLA.....");
-  //   }
-  //   
-  //   return $lid;
-  // }
+  protected function createLocalUser()
+  {
+    $lid = null;
+    
+    if ($this->accessScope() == 'private') {
+      // Build the user
+      $user = $this->buildLocalUser();
+      
+      // Build query and run it
+      $query = "INSERT INTO odm_user (username, password, department, Email,last_name, first_name) VALUES(
+        '". mysql_real_escape_string($user['username'])."', 
+        md5('". mysql_real_escape_string($user['password']) ."'), 
+        '" . mysql_real_escape_string($user['department'])."',
+        '" . mysql_real_escape_string($user['email'])."',
+        '" . mysql_real_escape_string($user['last_name']) . "',
+        '" . mysql_real_escape_string($user['first_name']) . "' )";
+      
+      $result = mysql_query($query, $this->connection);
+      
+      // Get user id
+      $lid = mysql_insert_id($this->connection);
+      
+      // Set the admin status for this user
+      if ($lid > 0) {
+        $query = "INSERT INTO odm_admin (id, admin) VALUES(
+          '" . $lid . "', 
+          '" . $this->isAdmin() . "')";
+        
+        $result = mysql_query($query, $this->connection);
+      }
+    }
+    
+    return $lid;
+  }
+  
+  /**
+   * Build a local user for creation
+   *
+   * @return associative array
+   */
+  protected function buildLocalUser()
+  {
+    $user_data = Array(
+      'first_name' => $this->name,
+      'last_name'  => $this->surname,
+      'username'   => $this->uid,
+      'password'   => $this->generatePassword(),
+      'email'      => $this->email,
+      'department' => $this->getDefaultDepartmentId()
+    );
+    
+    return $user_data;
+  }
+  
+  /**
+   * Return the department ID that should be assigned
+   * to a user by default
+   * (Opendocman requires one department at least so
+   * the return value should never be null)
+   *
+   * @return a department ID if found, null otherwise
+   */
+  protected function getDefaultDepartmentId()
+  {
+    $query = "SELECT id FROM odm_department LIMIT 1";
+    $result = mysql_query($query, $this->connection);
+    $result = mysql_fetch_assoc($result);
+    
+    if ($result && $result['id']) {
+      return $result['id'];
+    }
+    
+    return null;
+  }
+  
+  /**
+   * Return wether the user is admin or not
+   * If the user is the owner of the app or at least Admin
+   * for each organization, then it is given the role of 'Admin'.
+   * Return 'User' role otherwise
+   *
+   * @return 1 if admin, 0 otherwise
+   */
+  protected function isAdmin() {
+    $admin = 0; // User
+    
+    if ($this->app_owner) {
+      $admin = 1; // Admin
+    } else {
+      foreach ($this->organizations as $organization) {
+        if ($organization['role'] == 'Admin' || $organization['role'] == 'Super Admin') {
+          $admin = 1;
+        } else {
+          $admin = 0;
+        }
+      }
+    }
+    
+    return $admin;
+  }
   
   /**
    * Get the ID of a local user via Maestrano UID lookup
    *
    * @return a user ID if found, null otherwise
    */
-  // protected function getLocalIdByUid()
-  // {
-  //   $result = $this->connection->query("SELECT ID FROM user WHERE mno_uid = {$this->connection->quote($this->uid)} LIMIT 1")->fetch();
-  //   
-  //   if ($result && $result['ID']) {
-  //     return $result['ID'];
-  //   }
-  //   
-  //   return null;
-  // }
+  protected function getLocalIdByUid()
+  {
+    $query = "SELECT id FROM odm_user WHERE mno_uid = '" . mysql_real_escape_string($this->uid) . "'";
+    $result = mysql_query($query, $this->connection);
+    $result = mysql_fetch_assoc($result);
+    
+    if ($result && $result['id']) {
+      return $result['id'];
+    }
+    
+    return null;
+  }
   
   /**
    * Get the ID of a local user via email lookup
    *
    * @return a user ID if found, null otherwise
    */
-  // protected function getLocalIdByEmail()
-  // {
-  //   $result = $this->connection->query("SELECT ID FROM user WHERE email = {$this->connection->quote($this->email)} LIMIT 1")->fetch();
-  //   
-  //   if ($result && $result['ID']) {
-  //     return $result['ID'];
-  //   }
-  //   
-  //   return null;
-  // }
+  protected function getLocalIdByEmail()
+  {
+    $query = "SELECT id FROM odm_user WHERE Email = '" . mysql_real_escape_string($this->email) . "'";
+    $result = mysql_query($query, $this->connection);
+    $result = mysql_fetch_assoc($result);
+    
+    if ($result && $result['id']) {
+      return $result['id'];
+    }
+    
+    return null;
+  }
   
   /**
    * Set all 'soft' details on the user (like name, surname, email)
@@ -122,28 +200,40 @@ class MnoSsoUser extends MnoSsoBaseUser
    *
    * @return boolean whether the user was synced or not
    */
-   // protected function syncLocalDetails()
-   // {
-   //   if($this->local_id) {
-   //     $upd = $this->connection->query("UPDATE user SET name = {$this->connection->quote($this->name . ' ' . $this->surname)}, email = {$this->connection->quote($this->email)} WHERE ID = $this->local_id");
-   //     return $upd;
-   //   }
-   //   
-   //   return false;
-   // }
+   protected function syncLocalDetails()
+   {
+     if($this->local_id) {
+       
+       $query = "UPDATE odm_user 
+         SET Email = '" . mysql_real_escape_string($this->email) . "',
+         username = '" . mysql_real_escape_string($this->uid) . "',
+         last_name = '" . mysql_real_escape_string($this->surname) . "',
+         first_name = '" . mysql_real_escape_string($this->name) . "',
+         WHERE id = '" . mysql_real_escape_string($this->local_id) . "'";
+       
+       $upd = mysql_query($query, $this->connection);
+       return $upd;
+     }
+     
+     return false;
+   }
   
   /**
    * Set the Maestrano UID on a local user via id lookup
    *
    * @return a user ID if found, null otherwise
    */
-  // protected function setLocalUid()
-  // {
-  //   if($this->local_id) {
-  //     $upd = $this->connection->query("UPDATE user SET mno_uid = {$this->connection->quote($this->uid)} WHERE ID = $this->local_id");
-  //     return $upd;
-  //   }
-  //   
-  //   return false;
-  // }
+  protected function setLocalUid()
+  {
+    if($this->local_id) {
+      $query = "UPDATE odm_user 
+        SET mno_uid = '" . mysql_real_escape_string($this->uid) . "',
+        WHERE id = '" . mysql_real_escape_string($this->local_id) . "'";
+      
+      $upd = mysql_query($query, $this->connection);
+      return $upd;
+    }
+    
+    return false;
+  }
 }
